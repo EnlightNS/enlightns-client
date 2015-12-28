3d367cd6a07646e91af0d2e939a9dc2e5f374c16
syawar <you@example.com>
2015-12-28 12:05:59 -0500

    Form event listening complete

---
+++ app/js/login.js
@@ -0,0 +1,11 @@
+//load the main process
+var remote = require('remote')
+
+
+//form event listner
+var form = document.querySelector('form');
+form.addEventListener('submit', function(ev){
+  var login = form.querySelector('input[name="login"]');
+  var pass = form.querySelector('input[name="password"]');
+
+});
---
+++ main.js
@@ -0,0 +1,24 @@
+/**
+ * Created by syawar on 26/12/15.
+ */
+var app = require('app')
+
+var BrowserWindow = require('browser-window')
+
+app.on('ready', function(){
+
+    //create the browser window
+    var mainWindow = new BrowserWindow({
+        width:800,
+        height:600,
+        icon: __dirname + '/app/img/logo.png'
+    });
+
+    // load the index.html
+    mainWindow.loadURL('file:///' + __dirname + '/app/index.html');
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+
+})
+
